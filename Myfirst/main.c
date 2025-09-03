49bdcbdb03d646fffd8a591d4b2ad1a77ea9c144
KwonJY0318 <wnsdud7530@naver.com>
2025-09-03 11:44:06 +0900

    초기 커밋.

---
+++ Myfirst/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello, World!\n");
+
+	return 0;
+}
+
+
+void test1(void)
+{
+
+	//정수
+	int age = 20;
+
+	//상수형 변수 (수정불가)
+	const double PI = 3.141592;
+
+	//문자
+	char myname[128] = "양현인";
+
+	//정수타입 4개저장하는 변수
+	int iarray[] = {1, 2, 3, 4};
+
+	age = age + 1;
+
+	//2M
+	double r = 2;
+
+	// 반지름 r 인 원의 면적
+	double area = PI * r * r;
+
+	//관계 연산자  '&&' '||' '~'
+	//반복문 'for' 'while' 'do ~ while' 
+	if (age > 10 && area > 5.0)
+	{
+
+
+	}
+	else
+	{
+
+	}
+
+
+
+
+
+}
+
+
